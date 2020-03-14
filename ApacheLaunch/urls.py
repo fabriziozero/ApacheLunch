fa6c0753f1c8a03c9c2adc0e3433a07fbb2c885d
delPuer <delpuertomiguel@fpuna.edu.py>
2020-03-14 18:24:27 -0300

    prueba de tutorial 10

--- ApacheLaunch/urls.py
+++ ApacheLaunch/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+
+from ApacheLaunch.views import saludo, despedida, dameFecha, calculaEdad
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/',admin.site.urls),
+    path('saludo/', saludo),
+    path('mano/', despedida),
+    path('fecha/', dameFecha),
+    path('edad/<int:edad>/<int:anho>/', calculaEdad)
 ]
 
